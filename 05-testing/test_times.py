991f67ea3a2e757ac2a9875916c9bb72d146f536
Syris Chan <chung.chan.18@ucl.ac.uk>
2019-10-31 11:58:35 +0000

    comment on what to test

--- 05-testing/test_times.py
+++ 05-testing/test_times.py
@@ -8,6 +8,7 @@ def test_given_input():
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected    
 
+
 test_given_input() #this runs the function 
 
 def test_class_time():
@@ -15,3 +16,15 @@ def test_class_time():
     short = times.time_range("2019-10-31 10:05:00", "2019-10-31 12:55:00", 3, 60)
     result = times.overlap_time(large,short)
     assert result == short
+'''
+ no overlapping times instead of years 1/1 - 1/2, 1/3 -1/4
+ gap time larger then range
+ two time ranges are exactly the same aka. overlap of identical times
+ overlapping across different dates 1/1 - 1/3, 1/2 1/4
+ what when 1st < 2nd
+ 2nd = Gap 1st
+ wrong format input
+ end date earlier then start date  t1 < t0
+ time is negative, BC
+ Different time format, such as American time format 
+'''
