37655e2aee5d2e78e5d6fb31cae57663f7aef459
Syris Chan <chung.chan.18@ucl.ac.uk>
2019-10-31 11:16:47 +0000

    Answers UCL-RITS/rsd-classwork-2019#18 adding test_times.py

--- 05-testing/times.py
+++ 05-testing/times.py
@@ -1,12 +1,21 @@
 import datetime
 
+
 def time_range(t0, t1, n=1, g=0):
     t0_s = datetime.datetime.strptime(t0, "%Y-%m-%d %H:%M:%S")
     t1_s = datetime.datetime.strptime(t1, "%Y-%m-%d %H:%M:%S")
-    d = (t1_s - t0_s).total_seconds() / n + g * (1/n - 1)
-    sec_range = [(t0_s + datetime.timedelta(seconds=i * d + i * g),
-                  t0_s + datetime.timedelta(seconds=(i + 1) * d + i * g)) for i in range(n)]
-    return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
+    d = (t1_s - t0_s).total_seconds() / n + g * (1 / n - 1)
+    sec_range = [
+        (
+            t0_s + datetime.timedelta(seconds=i * d + i * g),
+            t0_s + datetime.timedelta(seconds=(i + 1) * d + i * g),
+        )
+        for i in range(n)
+    ]
+    return [
+        (ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S"))
+        for ta, tb in sec_range
+    ]
 
 
 def overlap_time(obs1, obs2):
